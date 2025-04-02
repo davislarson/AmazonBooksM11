8db9f0a902f633e1e55392d4ec7c6321eaf100b4
Davis Larson <davislarson01@gmail.com>
2025-04-02 13:00:30 -0600

    allow frontend link

--- backend/AmazonBooksM11/Program.cs
+++ backend/AmazonBooksM11/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("CorsPolicy",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000, https://agreeable-hill-082c3771e.6.azurestaticapps.net")
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
